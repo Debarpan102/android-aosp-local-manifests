eb8579b85a64a80f63e235e175721975b641753b
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-12-16 20:05:48 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -50,7 +50,8 @@ echo "========================================================================"
 
 sudo apt update && sudo apt upgrade -y
 sudo apt update && sudo apt install libc6-dev
-sudo apt update && sudo apt install openssl libssl-dev
+sudo apt install openssl libssl-dev && sudo apt install libssl-dev
+sudo apt-get install -y libfl-dev
 
 
 echo "========================================================================"
