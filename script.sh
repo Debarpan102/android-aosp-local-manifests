f1fc132d8bce23df0e04e3b6b314e017da7b8848
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-12-31 14:25:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ rm -rf hardware/oplus
 rm -rf device/oneplus
 rm -rf vendor/oneplus
 rm -rf vendor/oplus
+rm -rf vendor/bcr
 # rm -rf vendor/lineage-priv/keys/
 # rm -rf packages/apps/ViMusic
 # rm -rf packages/apps/Droid-ify
