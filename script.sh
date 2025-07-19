6a514c680efca69c57432c0476fd382a61e3d6d9
Debarpan102 <debarpan102github@gmail.com>
2025-07-20 01:50:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ echo "========================================================================"
 echo "DELETED DIRECTORIES"
 echo "========================================================================"
 
-repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 echo "========================================================================"
 echo "REPO INITIALIZED AND SYNCED"
@@ -37,12 +37,12 @@ rm -rf device/oneplus
 rm -rf vendor/oneplus
 rm -rf vendor/oplus
 
-git clone https://github.com/Debarpan102/android_device_realme_ice.git -b 16.0-yaap device/realme/ice
-git clone https://github.com/Debarpan102/android_device_oneplus_sm8350-common.git -b 16.0-yaap device/oneplus/sm8350-common
+git clone https://github.com/Infinity-X-Devices/device_realme_ice.git -b 16 device/realme/ice
+git clone https://github.com/Debarpan102/android_device_oneplus_sm8350-common.git -b 3.0-infinityX device/oneplus/sm8350-common
 git clone --depth 1 https://github.com/Debarpan102/proprietary_vendor_realme_ice.git -b lineage-22.2 vendor/realme/ice
 git clone --depth 1 https://github.com/Debarpan102/proprietary_vendor_oneplus_sm8350-common.git -b 16.0 vendor/oneplus/sm8350-common
-git clone --depth 1 https://github.com/Debarpan102/android_hardware_oplus.git -b 16.0-yaap hardware/oplus
-git clone --depth 1 https://github.com/Debarpan102/kernel_oplus_RMX3461.git -b stable-r1 kernel/oplus/RMX3461
+git clone --depth 1 https://github.com/Debarpan102/android_hardware_oplus.git -b 3.0-infinityX hardware/oplus
+git clone --depth 1 https://github.com/Debarpan102/kernel_oplus_RMX3461.git -b stable-r2 kernel/oplus/RMX3461
 echo "========================================================================"
 echo "CLONED REPOS"
 echo "========================================================================"
@@ -56,15 +56,6 @@ sudo apt update && sudo apt upgrade -y
 sudo apt update && sudo apt install libc6-dev
 sudo apt install openssl libssl-dev -y && sudo apt install libssl-dev -y
 sudo apt-get install libfl-dev -y
-
-export CLANG_PATH=$PWD/prebuilts/clang/host/linux-x86/clang-r522817/bin
-export PATH=$CLANG_PATH:$PATH
-alias clang=$CLANG_PATH/clang
-alias clang++=$CLANG_PATH/clang++
-alias ld.lld=$CLANG_PATH/ld.lld
-export CC=$CLANG_PATH/clang
-export CXX=$CLANG_PATH/clang++
-export LD=$CLANG_PATH/ld.lld
 
 echo "========================================================================"
 echo "SYSTEM UPGRADED"
@@ -82,6 +73,6 @@ echo "========================================================================"
 
 # yaap
 . build/envsetup.sh
-lunch yaap_ice-user
-TARGET_BUILD_GAPPS=true m yaap
+lunch infinity_ice-user
+m bacon
 
