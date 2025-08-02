54d26b724f9b4a8d2be42cf18114f52fa08f7e0a
Debarpan102 <debarpan102github@gmail.com>
2025-08-02 15:37:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -38,12 +38,12 @@ rm -rf device/oneplus
 rm -rf vendor/oneplus
 rm -rf vendor/oplus
 
-git clone https://github.com/Debarpan102/android_device_realme_ice.git -b test device/realme/ice
+git clone https://github.com/Debarpan102/android_device_realme_ice.git -b 16.0-yaap device/realme/ice
 git clone https://github.com/Debarpan102/android_device_oneplus_sm8350-common.git -b 16.0-yaap device/oneplus/sm8350-common
 git clone --depth 1 https://github.com/Debarpan102/proprietary_vendor_realme_ice.git -b lineage-22.2 vendor/realme/ice
 git clone --depth 1 https://github.com/Debarpan102/proprietary_vendor_oneplus_sm8350-common.git -b 16.0 vendor/oneplus/sm8350-common
 git clone --depth 1 https://github.com/Debarpan102/android_hardware_oplus.git -b 16.0-yaap hardware/oplus
-git clone --depth 1 https://github.com/Debarpan102/kernel_oplus_RMX3461.git -b sixteen kernel/oneplus/sm8350
+git clone --depth 1 https://github.com/Debarpan102/kernel_oneplus_sm8350.git -b sixteen kernel/oneplus/sm8350
 echo "========================================================================"
 echo "CLONED REPOS"
 echo "========================================================================"
