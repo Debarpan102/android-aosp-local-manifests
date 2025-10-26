22a4b6fadf4703db14545643d471ff8868a5fd58
Debarpan102 <debarpan102github@gmail.com>
2025-10-27 02:49:03 +0530

    Change branch names in git clone commands

--- script.sh
+++ script.sh
@@ -30,9 +30,9 @@ echo "========================================================================"
 git clone https://github.com/Debarpan102/android_device_realme_ice.git -b 16.0-yaap device/realme/ice
 git clone https://github.com/Debarpan102/android_device_oneplus_sm8350-common.git -b 16.0-yaap device/oneplus/sm8350-common
 git clone https://github.com/Debarpan102/proprietary_vendor_realme_ice.git -b 16.0 vendor/realme/ice
-git clone https://github.com/Debarpan102/proprietary_vendor_oneplus_sm8350-common.git -b 16.0 vendor/oneplus/sm8350-common
+git clone https://github.com/Debarpan102/proprietary_vendor_oneplus_sm8350-common.git -b sixteen vendor/oneplus/sm8350-common
 git clone https://github.com/Debarpan102/android_hardware_oplus.git -b sixteen hardware/oplus
-git clone https://github.com/Debarpan102/kernel_oneplus_sm8350.git -b sixteen-stg kernel/oneplus/sm8350
+git clone https://github.com/Debarpan102/kernel_oneplus_sm8350.git -b sixteen kernel/oneplus/sm8350
 
 echo "========================================================================"
 echo "CLONED REPOS"
